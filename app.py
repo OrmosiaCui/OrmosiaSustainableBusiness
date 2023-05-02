85e3a46ef2ca394482b4edf4e9d734fba31a2b32
Xinlei, Ormosia <114370154+OrmosiaCui@users.noreply.github.com>
2023-05-02 15:31:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 
-app=Flask(__name__)
+app=Flask(__name__, static_url_path='/static')
 GB=pickle.load(open('savedmodelGB.sav','rb'))
 
 @app.route('/')
